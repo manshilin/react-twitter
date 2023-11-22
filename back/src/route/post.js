78d50b15b158ce0bf18a9d297d418cafb2bbab5a
Sergii Manshylin <sergeymanshilin@gmail.com>
2023-11-22 14:34:08 +0200

    react-twitter-2

--- back/src/route/post.js
+++ back/src/route/post.js
@@ -55,70 +55,70 @@ router.post('/post-create', function (req, res) {
   }
 })
 
-// router.get('/post-list', function (req, res) {
-//   try {
-//     const list = Post.getList()
+router.get('/post-list', function (req, res) {
+  try {
+    const list = Post.getList()
 
-//     if (list.length === 0) {
-//       return res.status(200).json({
-//         list: [],
-//       })
-//     }
+    if (list.length === 0) {
+      return res.status(200).json({
+        list: [],
+      })
+    }
 
-//     return res.status(200).json({
-//       list: list.map(({ id, username, text, date }) => ({
-//         id,
-//         username,
-//         text,
-//         date,
-//       })),
-//     })
-//   } catch (e) {
-//     return res.status(400).json({
-//       message: e.message,
-//     })
-//   }
-// })
+    return res.status(200).json({
+      list: list.map(({ id, username, text, date }) => ({
+        id,
+        username,
+        text,
+        date,
+      })),
+    })
+  } catch (e) {
+    return res.status(400).json({
+      message: e.message,
+    })
+  }
+})
 
-// router.get('/post-item', function (req, res) {
-//   try {
-//     const { id } = req.query
+router.get('/post-item', function (req, res) {
+  try {
+    const { id } = req.query
 
-//     if (!id) {
-//       return res.status(400).json({
-//         message: 'Потрібно передати ID поста',
-//       })
-//     }
+    if (!id) {
+      return res.status(400).json({
+        message: 'Потрібно передати ID поста',
+      })
+    }
 
-//     const post = Post.getById(Number(id))
+    const post = Post.getById(Number(id))
 
-//     if (!post) {
-//       return res.status(400).json({
-//         message: 'Пост з таким ID не існує',
-//       })
-//     }
+    if (!post) {
+      return res.status(400).json({
+        message: 'Пост з таким ID не існує',
+      })
+    }
 
-//     return res.status(200).json({
-//       post: {
-//         id: post.id,
-//         text: post.text,
-//         username: post.username,
-//         date: post.date,
+    return res.status(200).json({
+      post: {
+        id: post.id,
+        text: post.text,
+        username: post.username,
+        date: post.date,
 
-//         reply: post.reply.map((reply) => ({
-//           id: reply.id,
-//           text: reply.text,
-//           username: reply.username,
-//           date: reply.date,
-//         })),
-//       },
-//     })
-//   } catch (e) {
-//     return res.status(400).json({
-//       message: e.message,
-//     })
-//   }
-// })
+        reply: post.reply.map((reply) => ({
+          id: reply.id,
+          text: reply.text,
+          username: reply.username,
+          date: reply.date,
+        })),
+      },
+    })
+  } catch (e) {
+    return res.status(400).json({
+      message: e.message,
+    })
+  }
+})
 
-// Підключаємо роутер до бек-енду
+//Підключаємо роутер до бек-енду
 module.exports = router
